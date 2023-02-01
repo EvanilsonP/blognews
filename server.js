8001f4113d8a52df3db371050ca34f3252e43446
EvanilsonP <pevanilson99@gmail.com>
2023-02-01 10:26:41 -0300

    Middleware

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandling');
+
+// custom middleware logger
+app.use(logger);
+// Cross origin resource sharing
+const whiteList = ['http://www.yoursite.com', 'http://127.0.0.1:5500'];
+const corsOption = {
+    origin: (origin, callback) => {
+        if(whiteList.indexOf(origin) !== -1 || origin) { // if the domain is not in the white list
+            callback(null, true); // null: error, true: domain allowed
+        } else {
+            callback(new Error('Not allowed by CORS.'));
+        }
+    },
+    optionSucessStatus: 200
+}
+app.use(cors(corsOption));
+
+// bult-in middleware to handle urlenconded data
+// in other words, form data:
+// 'content-type': application/x-www-form-urlenconded
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     // res.sendFile('./views/index.html', { root: __dirname});
@@ -23,8 +53,17 @@ app.get('/hello(.html)?', (req, res, next) => {
     res.send('Hello World!!');
 });
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')) {
+        res.json({ error: '404 not found'});
+    } else {
+        res.type('txt').send('404 not found.');
+    }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
