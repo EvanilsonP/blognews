8962c950a982c5483e5098353073b96836eaf98f
EvanilsonP <pevanilson99@gmail.com>
2023-03-01 11:59:59 -0300

    connecting to the database

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const db = require('./database/db');
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(routes);
+db.database();
 
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`)});
